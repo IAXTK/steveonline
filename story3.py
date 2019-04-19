e2c68f2c99032ca922227412d09a3a3bd6ea7426
IAXTK <IAXTK>
2019-04-19 15:49:45 +0000

    Reduce chars per line in makeStory()

--- story3.py
+++ story3.py
@@ -4,16 +4,16 @@ from PIL import ImageDraw
 import textwrap
 
 
-def makeStoryImage(text, outputPath):
-    text = textwrap.wrap(text, 35) #sanitize text with 
+def makeStory(text, outputPath):
+    text = textwrap.wrap(text, 30) #sanitize text with 
     text = '\n'.join(text)         #newlines and stuff
 
     img = Image.open("background.png") #open prerendered background
     img = img.convert("RGB") #make it a jpeg (remove alpha)
     draw = ImageDraw.Draw(img) #drawing surface
     font = ImageFont.truetype("arial.ttf", 69) #make this whatever font
-    draw.text((50, 90),text,(0,0,0),font=font) #draw the text
+    draw.text((50, 150),text,(0,0,0),font=font) #draw the text
     img.save(outputPath) #save it
 
 if __name__ == '__main__':
-    makeStoryImage("This is a demonstration of story3.py. Im guessing that this is pretty close to the original before i lost it....", 'demo.jpg')
+    makeStory("This is a demonstration of story3.py. Im guessing that this is pretty close to the original before i lost it....", 'demo.jpg')
